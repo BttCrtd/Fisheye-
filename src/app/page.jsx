649a9d9260ffd06b6adfa210006a0fc268d3695a
BttCrtd <courtadon.baptiste@gmail.com>
2026-06-10 22:13:44 +0200

    Header component

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Header from "@/components/Header/Header";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <Header />
+      <main className={styles.main}>
+        
+      </main>
+    </div>
+  );
+}
