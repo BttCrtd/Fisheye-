cf2f1992ce67d93696283d5495b15c12237ed95e
BttCrtd <courtadon.baptiste@gmail.com>
2026-06-12 11:18:52 +0200

    Counter like

--- src/app/photographer/[id]/page.jsx
+++ src/app/photographer/[id]/page.jsx
@@ -3,11 +3,14 @@ import { getPhotographer, getAllMediasForPhotographer, } from "@/app/lib/prisma-
 import Header from "@/components/Header/Header";
 import PhotographerHeader from "@/components/PhotographerHeader/PhotographerHeader";
 import PhotoCard from "@/components/PhotoCard/PhotoCard";
+import { FaHeart } from "react-icons/fa";
 
 export default async function Page({ params }) {
   const { id } = await params;
   const photographer = await getPhotographer(id);
   const medias = await getAllMediasForPhotographer(id);
+
+  const totalLikes = medias.reduce((count, media) => count + media.likes, 0);
   return (
     <div className="page">
       <Header />
@@ -26,6 +29,13 @@ export default async function Page({ params }) {
             ))}
           </div>
         </section>
+         <div className="cta">
+          <div className="like">
+            <p className="counter">{totalLikes}</p>
+            <FaHeart className="heart-icon" />
+          </div>
+          <p className="price">{photographer.price}€/jour</p>
+        </div>
       </main>
     </div>
   );
